245008cdd6f26880318e1e393e5adc58a041edb6
Jörg Wasmeier <j.wasmeier@faba.de>
2017-12-19 12:57:58 +0100

    Updates

--- src/A_CLI.ts
+++ src/A_CLI.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import FabaCore from "@fabalous/core/FabaCore";
 import FabalousStore from "./FabalousStore";
 import InitFabalousEvent from "./event/InitFabalousEvent";
@@ -5,7 +6,6 @@ import FabalousMediator from "./FabalousMediator";
 import FabaStore from "@fabalous/core/store/FabaStore";
 
 declare var process;
-
 
 class A_CLI extends FabaCore{
     constructor(store){
